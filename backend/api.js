46914a817d053474e4a5fe9c81c9e43d34b83d86
Tychette <201940472@cegepsherbrooke.qc.ca>
2023-12-04 00:12:48 -0500

    Projet Final

--- backend/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const noteController = require('./noteController');
-
-router.post('/notes', noteController.createNote);
-router.get('/notes', noteController.getAllNotes);
-
-module.exports = router;
--- frontend/src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import noteReducer from '../reducers/noteReducer';
-
-const store = createStore(noteReducer, applyMiddleware(thunk));
-
-export default store;
